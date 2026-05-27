5ed3d6927d42f36bbbc3ce0de764c4b69e7cd385
Katherine <katerynaunik@gmail.com>
2026-05-27 23:34:25 +0300

    done

--- src/components/FormComponent/FormComponent.js
+++ src/components/FormComponent/FormComponent.js
@@ -1,8 +1,44 @@
 import './FormComponent.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 export function FormComponent(props) {
 
+  const [state, setState] = useState ({
+    email: '',
+    password: '',
+    emailValid: false,
+    passwordValid: false
+  });
+
+  function validateEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+  function validatePassword(password) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(password);
+  }
+
+  function onEmailChange(e) {
+    var val = e.target.value;
+    var valid = validateEmail(val);
+    setState({...state, email: val, emailValid: valid});
+  }
+  function onPasswordChange(e) {
+    var val = e.target.value;
+    var valid = validatePassword(val);
+    setState({...state, password: val, passwordValid: valid});
+  }
+
+  function handleSubmit(e) {
+    if (state.emailValid === true && state.passwordValid === true) {
+      alert("Success!");
+    }
+    else {
+      e.preventDefault();
+    }
+  }
+
+  var emailValidView = state.emailValid===true?"valid":"invalid";
+  var passwordValidView = state.passwordValid===true?"valid":"invalid";
 
   return <>
   <div className="FormComponent">
@@ -11,11 +47,15 @@ export function FormComponent(props) {
     </div>
 
     <div className="right-part">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="login-and-password">
           <div className="form-settings">
             <label>Електронна пошта або мобільний телефон</label>
-            <input></input>
+            <input
+            type="text"
+            value={state.email}
+            onChange={onEmailChange}
+            ></input>
             <p className="agreeing">
               By entering your mobile number and one-time code sign-in option,
               you agree to receive a one-time verification code via SMS from IKEA.
@@ -26,7 +66,10 @@ export function FormComponent(props) {
 
         <div className="form-settings">
           <label>Пароль</label>
-          <input></input>
+          <input
+          type="password"
+          value={state.password}
+          onChange={onPasswordChange}></input>
           <p className="password-forgot">Забули свій пароль?</p>
         </div>
 
@@ -35,7 +78,8 @@ export function FormComponent(props) {
           <label>Зберегти інформацію</label>
         </div>
 
-        <button className="continue">ПРОДОВЖИТИ</button>
+        <button className="continue"
+        type="submit">ПРОДОВЖИТИ</button>
 
         <div className="create-account">
           <p>У вас ще немає облікового запису? Створіть його:</p>
